128815ef61cbdfda823531f04fedde9246b71db6
Mayank Soni <sonimayank151@gmail.com>
2024-10-21 21:00:56 +0530

    Still in progress

--- config/allowedOrigins.mjs
+++ config/allowedOrigins.mjs
@@ -2,7 +2,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "https://www.gcfs.com",
   "https://gcfs.com",
-  "https://bank-client-front-end.vercel.app/"
+  "https://bank-client-back-end.vercel.app/"
 ];
 
 export default allowedOrigins;
